27cf673c3152901b165f941d0e9b42d936a3edb9
falco.weber <weber@ehealthexperts.de>
2019-08-14 15:25:41 +0200

    EKON-219 Anpassungen für Serverseitiges SessionResumption wieder eingemerged

--- tls/src/main/java/org/bouncycastle/jsse/provider/ProvSSLSessionContext.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/ProvSSLSessionContext.java
@@ -19,301 +19,6 @@ import org.bouncycastle.tls.SessionID;
 import org.bouncycastle.tls.TlsSession;
 import org.bouncycastle.tls.crypto.TlsCrypto;
 
-<<<<<<< HEAD
-class ProvSSLSessionContext implements SSLSessionContext {
-	private static Logger LOG = Logger.getLogger(ProvSSLSessionContext.class.getName());
-
-	private static final int provSessionCacheSize = PropertyUtils
-			.getIntegerSystemProperty("javax.net.ssl.sessionCacheSize", 0, 0, Integer.MAX_VALUE);
-
-	// NOTE: This is configured as a simple LRU cache using the "access order"
-	// constructor
-	@SuppressWarnings("serial")
-	protected final Map<SessionID, SessionEntry> sessionsByID = new LinkedHashMap<SessionID, SessionEntry>(16, 0.75f,
-			true) {
-		protected boolean removeEldestEntry(Map.Entry<SessionID, SessionEntry> eldest) {
-			boolean shouldRemove = sessionCacheSize > 0 && size() > sessionCacheSize;
-			if (shouldRemove) {
-				removeSessionByPeer(eldest.getValue());
-				eldest.getValue().get().invalidate();
-			}
-			return shouldRemove;
-		}
-	};
-	protected final Map<String, SessionEntry> sessionsByPeer = new HashMap<String, SessionEntry>();
-	protected final ReferenceQueue<ProvSSLSessionImpl> sessionsQueue = new ReferenceQueue<ProvSSLSessionImpl>();
-
-	protected final ProvSSLContextSpi sslContext;
-	protected final TlsCrypto crypto;
-
-	protected int sessionCacheSize = provSessionCacheSize;
-	protected int sessionTimeoutSeconds = 86400; // 24hrs (in seconds)
-
-	ProvSSLSessionContext(ProvSSLContextSpi sslContext, TlsCrypto crypto) {
-		this.sslContext = sslContext;
-		this.crypto = crypto;
-	}
-
-	ProvSSLContextSpi getSSLContext() {
-		return sslContext;
-	}
-
-	TlsCrypto getCrypto() {
-		return crypto;
-	}
-
-	synchronized ProvSSLSessionImpl getSessionImpl(byte[] sessionID) {
-		removeAllExpiredSessions();
-		return accessSession(mapGet(sessionsByID, makeSessionID(sessionID)));
-	}
-
-	synchronized ProvSSLSessionImpl getSessionImpl(String hostName, int port) {
-		removeAllExpiredSessions();
-
-		SessionEntry sessionEntry = mapGet(sessionsByPeer, makePeerKey(hostName, port));
-		ProvSSLSessionImpl session = accessSession(sessionEntry);
-		if (session != null) {
-			// NOTE: For the current simple cache implementation, need to 'access' the
-			// sessionByIDs entry
-			sessionsByID.get(sessionEntry.getSessionID());
-		}
-		return session;
-	}
-
-	synchronized ProvSSLSessionImpl reportSession(TlsSession tlsSession, String peerHost, int peerPort) {
-
-		if (sessionCacheSize > 0) {
-			SessionID sessionID = new SessionID(tlsSession.getSessionID());
-			SessionEntry sessionEntry = sessionsByID.get(sessionID);
-			ProvSSLSessionImpl session = sessionEntry == null ? null : sessionEntry.get();
-
-			if (session == null || session.getTlsSession() != tlsSession) {
-				session = new ProvSSLSessionImpl(this, tlsSession, peerHost, peerPort);
-				sessionEntry = new SessionEntry(sessionID, session, sessionsQueue);
-				sessionsByID.put(sessionID, sessionEntry);
-			}
-
-			mapAdd(sessionsByPeer, sessionEntry.getPeerKey(), sessionEntry);
-
-			return session;
-		} else {
-			tlsSession.invalidate();
-			return new ProvSSLSessionImpl(this, tlsSession, peerHost, peerPort);
-		}
-
-	}
-
-	public synchronized Enumeration<byte[]> getIds() {
-		removeAllExpiredSessions();
-
-		ArrayList<byte[]> ids = new ArrayList<byte[]>(sessionsByID.size());
-		for (SessionID sessionID : sessionsByID.keySet()) {
-			ids.add(sessionID.getBytes());
-		}
-		return Collections.enumeration(ids);
-	}
-
-	public SSLSession getSession(byte[] sessionID) {
-		if (sessionID == null) {
-			throw new NullPointerException("'sessionID' cannot be null");
-		}
-
-		return getSessionImpl(sessionID);
-	}
-
-	public synchronized int getSessionCacheSize() {
-		return sessionCacheSize;
-	}
-
-	public synchronized int getSessionTimeout() {
-		return sessionTimeoutSeconds;
-	}
-
-	public synchronized void setSessionCacheSize(int size) throws IllegalArgumentException {
-		if (sessionCacheSize == size) {
-			return;
-		}
-
-		if (size < 0) {
-			throw new IllegalArgumentException("'size' cannot be < 0");
-		}
-
-		this.sessionCacheSize = size;
-
-		removeAllExpiredSessions();
-
-		// Immediately remove LRU sessions in excess of the new limit
-		if (sessionCacheSize > 0) {
-			int currentSize = sessionsByID.size();
-			if (currentSize > sessionCacheSize) {
-				Iterator<SessionEntry> iter = sessionsByID.values().iterator();
-				while (iter.hasNext() && currentSize > sessionCacheSize) {
-					SessionEntry sessionEntry = iter.next();
-					iter.remove();
-					removeSessionByPeer(sessionEntry);
-					--currentSize;
-				}
-			}
-		}
-	}
-
-	public synchronized void setSessionTimeout(int seconds) throws IllegalArgumentException {
-		if (sessionTimeoutSeconds == seconds) {
-			return;
-		}
-
-		if (seconds < 0) {
-			throw new IllegalArgumentException("'seconds' cannot be < 0");
-		}
-
-		this.sessionTimeoutSeconds = seconds;
-
-		removeAllExpiredSessions();
-	}
-
-	private ProvSSLSessionImpl accessSession(SessionEntry sessionEntry) {
-		if (sessionEntry != null) {
-			ProvSSLSessionImpl session = sessionEntry.get();
-			if (session != null) {
-				long currentTimeMillis = System.currentTimeMillis();
-				if (!invalidateIfCreatedBefore(sessionEntry, getCreationTimeLimit(currentTimeMillis))) {
-					session.accessedAt(currentTimeMillis);
-					return session;
-				}
-			}
-
-			removeSession(sessionEntry);
-		}
-		return null;
-	}
-
-	private long getCreationTimeLimit(long expiryTimeMillis) {
-		return sessionTimeoutSeconds < 1 ? Long.MIN_VALUE : (expiryTimeMillis - 1000L * sessionTimeoutSeconds);
-	}
-
-	private boolean invalidateIfCreatedBefore(SessionEntry sessionEntry, long creationTimeLimit) {
-		ProvSSLSessionImpl session = sessionEntry.get();
-		if (session == null) {
-			return true;
-		}
-		if (session.getCreationTime() < creationTimeLimit) {
-			session.invalidate();
-		}
-		return !session.isValid();
-	}
-
-	private void processQueue() {
-		int count = 0;
-
-		SessionEntry sessionEntry;
-		while ((sessionEntry = (SessionEntry) sessionsQueue.poll()) != null) {
-			removeSession(sessionEntry);
-			++count;
-		}
-
-		if (count > 0) {
-			LOG.fine("Processed " + count + " session entries (soft references) from the reference queue");
-		}
-	}
-
-	private void removeAllExpiredSessions() {
-		processQueue();
-
-		long creationTimeLimit = getCreationTimeLimit(System.currentTimeMillis());
-
-		Iterator<SessionEntry> iter = sessionsByID.values().iterator();
-		while (iter.hasNext()) {
-			SessionEntry sessionEntry = iter.next();
-			if (invalidateIfCreatedBefore(sessionEntry, creationTimeLimit)) {
-				iter.remove();
-				removeSessionByPeer(sessionEntry);
-			}
-		}
-	}
-
-	private void removeSession(SessionEntry sessionEntry) {
-		boolean remove = mapRemove(sessionsByID, sessionEntry.getSessionID(), sessionEntry);
-
-		remove |= removeSessionByPeer(sessionEntry);
-		if (remove && sessionEntry.get() != null) {
-			sessionEntry.get().invalidate();
-		}
-	}
-
-	private boolean removeSessionByPeer(SessionEntry sessionEntry) {
-		return mapRemove(sessionsByPeer, sessionEntry.getPeerKey(), sessionEntry);
-	}
-
-	private static String makePeerKey(ProvSSLSessionImpl session) {
-		return session == null ? null : makePeerKey(session.getPeerHost(), session.getPeerPort());
-	}
-
-	private static String makePeerKey(String hostName, int port) {
-		return (hostName == null || port < 0) ? null
-				: (hostName + ':' + Integer.toString(port)).toLowerCase(Locale.ENGLISH);
-	}
-
-	private static SessionID makeSessionID(byte[] sessionID) {
-		return (sessionID == null || sessionID.length < 1) ? null : new SessionID(sessionID);
-	}
-
-	private static <K, V> void mapAdd(Map<K, V> map, K key, V value) {
-		if (map == null || value == null) {
-			throw new NullPointerException();
-		}
-		if (key != null) {
-			map.put(key, value);
-		}
-	}
-
-	private static <K, V> V mapGet(Map<K, V> map, K key) {
-		if (map == null) {
-			throw new NullPointerException();
-		}
-		return key == null ? null : map.get(key);
-	}
-
-	private static <K, V> boolean mapRemove(Map<K, V> map, K key, V value) {
-		if (map == null || value == null) {
-			throw new NullPointerException();
-		}
-		if (key != null) {
-			// TODO[jsse] From 1.8 there is a 2-argument remove method to accomplish this
-			V removed = map.remove(key);
-			if (removed == value) {
-				return true;
-			}
-			if (removed != null) {
-				map.put(key, removed);
-			}
-		}
-		return false;
-	}
-
-	private static final class SessionEntry extends SoftReference<ProvSSLSessionImpl> {
-		private final SessionID sessionID;
-		private final String peerKey;
-
-		SessionEntry(SessionID sessionID, ProvSSLSessionImpl session, ReferenceQueue<ProvSSLSessionImpl> queue) {
-			super(session, queue);
-
-			if (sessionID == null || session == null || queue == null) {
-				throw new NullPointerException();
-			}
-
-			this.sessionID = sessionID;
-			this.peerKey = makePeerKey(session);
-		}
-
-		public String getPeerKey() {
-			return peerKey;
-		}
-
-		public SessionID getSessionID() {
-			return sessionID;
-		}
-	}
-=======
 class ProvSSLSessionContext
     implements SSLSessionContext
 {
@@ -332,6 +37,7 @@ class ProvSSLSessionContext
             if (shouldRemove)
             {
                 removeSessionByPeer(eldest.getValue());
+				eldest.getValue().get().invalidate();
             }
             return shouldRemove;
         }
@@ -365,6 +71,7 @@ class ProvSSLSessionContext
     {
         processQueue();
 
+        removeAllExpiredSessions();
         return accessSession(mapGet(sessionsByID, makeSessionID(sessionID)));
     }
 
@@ -385,7 +92,7 @@ class ProvSSLSessionContext
     synchronized void removeSession(byte[] sessionID)
     {
         SessionEntry sessionEntry = mapRemove(sessionsByID, makeSessionID(sessionID));
-        if (null != sessionEntry)
+        if (null != sessionEntry && sessionEntry.get() != null)
         {
             removeSessionByPeer(sessionEntry);
         }
@@ -394,29 +101,37 @@ class ProvSSLSessionContext
     synchronized ProvSSLSession reportSession(String peerHost, int peerPort, TlsSession tlsSession,
         JsseSessionParameters jsseSessionParameters)
     {
-        processQueue();
+	    if(sessionCacheSize > 0)
+    	{
+            processQueue();
 
-        SessionID sessionID = makeSessionID(tlsSession.getSessionID());
-        SessionEntry sessionEntry = mapGet(sessionsByID, sessionID);
+            SessionID sessionID = makeSessionID(tlsSession.getSessionID());
+            SessionEntry sessionEntry = mapGet(sessionsByID, sessionID);
 
-        ProvSSLSession session = sessionEntry == null ? null : sessionEntry.get();
-        if (null == session || session.getTlsSession() != tlsSession)
-        {
-            session = new ProvSSLSession(this, peerHost, peerPort, tlsSession, jsseSessionParameters);
-
-            if (null != sessionID)
+            ProvSSLSession session = sessionEntry == null ? null : sessionEntry.get();
+            if (null == session || session.getTlsSession() != tlsSession)
             {
-                sessionEntry = new SessionEntry(sessionID, session, sessionsQueue);
-                sessionsByID.put(sessionID, sessionEntry);
+                session = new ProvSSLSession(this, peerHost, peerPort, tlsSession, jsseSessionParameters);
+    
+                if (null != sessionID)
+                {
+                    sessionEntry = new SessionEntry(sessionID, session, sessionsQueue);
+                    sessionsByID.put(sessionID, sessionEntry);
+                }
             }
-        }
 
-        if (null != sessionEntry)
+            if (null != sessionEntry)
+            {
+                mapAdd(sessionsByPeer, sessionEntry.getPeerKey(), sessionEntry);
+            }
+
+            return session;
+        }
+    	else 
         {
-            mapAdd(sessionsByPeer, sessionEntry.getPeerKey(), sessionEntry);
-        }
-
-        return session;
+            tlsSession.invalidate();
+            return new ProvSSLSession(this, peerHost, peerPort, tlsSession, jsseSessionParameters);
+        }		
     }
 
     public synchronized Enumeration<byte[]> getIds()
@@ -684,5 +399,4 @@ class ProvSSLSessionContext
             return sessionID;
         }
     }
->>>>>>> r1rv61
 }
